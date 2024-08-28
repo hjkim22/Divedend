b6217eaa266ae2b765a3e02df92bf868608ee78a
hjkim <hjhjgudwls1022@gmail.com>
2024-08-29 01:39:44 +0900

    setting

---
+++ src/test/java/com/divedend/DivedendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divedend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivedendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
